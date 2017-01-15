d501a0980d3eff45e7b676106908c579742bc811
linesh <linesh.simpcity@gmail.com>
2017-01-15 18:33:59 +0800

    [#3] Use window.location to get current url of the active tab

--- src/peak.main.js
+++ src/peak.main.js
@@ -1,6 +1,4 @@
 console.log('content script is working')
-chrome.runtime.onMessage.addListener(request => {
-   "use strict";
-   console.log('received message from browser action');
-   console.log(request.currentURL);
-})
+
+let activeTabUrl = window.location;
+alert(activeTabUrl)
