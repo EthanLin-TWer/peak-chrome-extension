f616849cbfb6c1e24767502ba8ad7473b33b76d2
linesh <linesh.simpcity@gmail.com>
2017-01-15 22:45:05 +0800

    [#3] Implement AC1~3

--- src/peak.main.js
+++ src/peak.main.js
@@ -1,4 +1,12 @@
 console.log('content script is working')
 
-let activeTabUrl = window.location;
+let activeTabUrl = window.location.href
+let landTime = new Date().toString().match(/(\d{2}:\d{2}:\d{2})/)[1]
+
+if (activeTabUrl.includes('https://mail.google.com/mail/u/') ||
+   activeTabUrl.includes('https://github.com/linesh-simplicity')) {
+   alert('Welcome to ' + activeTabUrl)
+}
+
 console.log(activeTabUrl)
+console.log('land time: ' + landTime)
